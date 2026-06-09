8587add46408ec8baa68d759bfa47a93faecdbf7
Reid Poole <poole.reid@gmail.com>
2026-06-09 16:29:48 -0600

    Require authorization for user management

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create, :update, :destroy]
+  before_action :authenticate_request, except: :create
+  before_action :require_admin, only: [:index, :destroy]
+  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :authorize_user_access, only: [:show, :update]
 
   def index
     users = User.all
@@ -7,12 +11,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
    def show
-    user = User.find_by(id: params[:id])
-    if user
-      render json: UserSerializer.new(user)
-    else
-      render json: { error: 'User not found' }, status: :not_found
-    end
+    render json: UserSerializer.new(@user)
   end
 
    def create
@@ -35,29 +34,15 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
-    user = User.find_by(id: params[:id])
-
-    if user.nil?
-      render json: { error: 'User not found' }, status: :not_found
-      return
-    end
-
-    if user.update(user_params)
-      render json: UserSerializer.new(user).serializable_hash.to_json, status: :ok
+    if @user.update(user_params)
+      render json: UserSerializer.new(@user).serializable_hash.to_json, status: :ok
     else
-      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    user = User.find_by(id: params[:id])
-
-    if user.nil?
-      render json: { error: 'User not found' }, status: :not_found
-      return
-    end
-
-    if user.destroy
+    if @user.destroy
       render json: { message: 'User deleted' }, status: :ok
     end
   end
@@ -66,5 +51,16 @@ class Api::V1::UsersController < ApplicationController
 
    def user_params
     params.require(:user).permit(:host_name, :description, :first_name, :last_name, :email, :phone_number, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+    render json: { error: 'User not found' }, status: :not_found unless @user
+  end
+
+  def authorize_user_access
+    return if @current_user.admin? || @current_user.id == @user.id
+
+    render json: { error: 'Not Authorized' }, status: :forbidden
   end
 end
--- spec/requests/api/v1/user_update_and_destroy_spec.rb
+++ spec/requests/api/v1/user_update_and_destroy_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "update and destroy" do
     it "updates a user's data" do
       user1 = User.create!(first_name: "John", last_name: "Doe", email: "lame@gmail.com", password: "1234password", password_confirmation: "1234password")
 
-      patch api_v1_user_path(user1), params: { user: { first_name: "Jack", last_name: "Smith", email: "doop@gmail.com" }}
+      patch api_v1_user_path(user1), params: { user: { first_name: "Jack", last_name: "Smith", email: "doop@gmail.com" }}, headers: auth_headers(user1)
 
       json_response = JSON.parse(response.body)
 
@@ -18,7 +18,7 @@ RSpec.describe "update and destroy" do
     it "does not update a user's password if it can't find the user" do
       user1 = User.create!(first_name: "John", last_name: "Doe", email: "lame@gmail.com", password: "1234password", password_confirmation: "1234password")
 
-      patch '/api/v1/users/123456789', params: { user: { first_name: "Jack", last_name: "Smith", email: "doop@gmail.com" }}
+      patch '/api/v1/users/123456789', params: { user: { first_name: "Jack", last_name: "Smith", email: "doop@gmail.com" }}, headers: auth_headers(user1)
 
       json_response = JSON.parse(response.body)
 
@@ -29,7 +29,7 @@ RSpec.describe "update and destroy" do
     it "throws an error if the fields are missing" do
       user1 = User.create!(first_name: "John", last_name: "Doe", email: "lame@gmail.com", password: "1234password", password_confirmation: "1234password")
 
-      patch api_v1_user_path(user1), params: { user: { first_name: "", last_name: "", email: "" }}
+      patch api_v1_user_path(user1), params: { user: { first_name: "", last_name: "", email: "" }}, headers: auth_headers(user1)
 
       json_response = JSON.parse(response.body)
 
@@ -40,9 +40,9 @@ RSpec.describe "update and destroy" do
 
   describe "destroy" do
     it "destroys a user" do
-      user1 = User.create!(first_name: "John", last_name: "Doe", email: "lame@gmail.com", password: "1234password", password_confirmation: "1234password")
+      user1 = User.create!(first_name: "John", last_name: "Doe", email: "lame@gmail.com", password: "1234password", password_confirmation: "1234password", role: "admin")
 
-      delete api_v1_user_path(user1)
+      delete api_v1_user_path(user1), headers: auth_headers(user1)
 
       json_response = JSON.parse(response.body)
 
@@ -53,9 +53,9 @@ RSpec.describe "update and destroy" do
     end
 
     it "throws an error if the user can't be found" do
-      user1 = User.create!(first_name: "John", last_name: "Doe", email: "lame@gmail.com", password: "1234password", password_confirmation: "1234password")
+      user1 = User.create!(first_name: "John", last_name: "Doe", email: "lame@gmail.com", password: "1234password", password_confirmation: "1234password", role: "admin")
 
-      delete '/api/v1/users/123456789'
+      delete '/api/v1/users/123456789', headers: auth_headers(user1)
 
       json_response = JSON.parse(response.body)
 
@@ -63,5 +63,9 @@ RSpec.describe "update and destroy" do
       expect(response).to_not be_successful
       expect(json_response["error"]).to eq("User not found")
     end
+  end
+
+  def auth_headers(user)
+    { "Authorization" => "Bearer #{JsonWebTokenService.encode(user_id: user.id)}" }
   end
 end
